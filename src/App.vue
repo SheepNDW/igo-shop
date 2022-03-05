341c79ca9a8d24443690bdec98f6fb4b0af35c9f
SheepNDW <hank3681@gmail.com>
2022-03-05 15:47:51 +0800

    加入手寫樣式

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,4 @@
 
 <style lang="scss">
 @import '@/assets/styles/all.scss';
-@import url(https://fonts.googleapis.com/earlyaccess/cwtexyen.css);
-* {
-  font-family: 'cwTeXYen', sans-serif;
-}
 </style>
