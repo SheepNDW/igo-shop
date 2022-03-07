f479f8ff3d4c5e58d8dd8daed917192b06ce8948
SheepNDW <hank3681@gmail.com>
2022-03-07 12:21:54 +0800

    footer製作

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,35 +1,41 @@
 <template>
-  <div class="app-footer bg-dark">
-    <div class="container">
-      <footer class="py-3">
-        <ul
-          class="nav justify-content-center border-bottom pb-3 mb-3 ul-border"
+  <footer class="app-footer">
+    <div class="footer-info">
+      <div class="container">
+        <div
+          class="slogan d-flex flex-column justify-content-center align-items-center flex-lg-row justify-content-lg-between"
         >
-          <li class="nav-item">
-            <RouterLink to="/" class="nav-link px-2 text-muted"
-              >首頁</RouterLink
-            >
-          </li>
-          <li class="nav-item">
-            <RouterLink to="/about" class="nav-link px-2 text-muted"
-              >關於我們</RouterLink
-            >
-          </li>
-          <li class="nav-item">
-            <RouterLink to="/products" class="nav-link px-2 text-muted"
-              >線上商城</RouterLink
-            >
-          </li>
-          <li class="nav-item">
-            <RouterLink to="/admin" class="nav-link px-2 text-muted"
-              >進入後台</RouterLink
-            >
-          </li>
-        </ul>
-        <p class="text-center text-muted">&copy; 2022 Company, Inc</p>
-      </footer>
+          <a href="javascript:;">
+            <i class="material-icons-outlined">paid</i>
+            <span>價格實惠</span>
+          </a>
+          <a href="javascript:;">
+            <i class="material-icons-outlined">thumb_up</i>
+            <span>品質保證</span>
+          </a>
+          <a href="javascript:;">
+            <i class="material-icons-outlined">local_shipping</i>
+            <span>全台配送</span>
+          </a>
+        </div>
+        <!-- 版權資訊 -->
+        <div class="copyright">
+          <p>
+            <RouterLink to="/">進入首頁</RouterLink>
+            <RouterLink to="/go">認識圍棋</RouterLink>
+            <RouterLink to="/products">線上商城</RouterLink>
+            <RouterLink to="/about">關於我們</RouterLink>
+          </p>
+          <p>
+            <RouterLink to="/">CopyRight © 碁人館</RouterLink>
+            <RouterLink to="/admin">
+              <i class="material-icons-outlined">manage_accounts</i>進入後台
+            </RouterLink>
+          </p>
+        </div>
+      </div>
     </div>
-  </div>
+  </footer>
 </template>
 
 <script>
@@ -39,7 +45,71 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.ul-border {
-  border-color: rgba(#dee2e6, 0.4) !important;
+.app-footer {
+  overflow: hidden;
+  background-color: #f5f5f5;
+  padding-top: 20px;
+  a {
+    text-decoration: none;
+  }
+
+  .footer-info {
+    background-color: #333;
+  }
+  .slogan {
+    height: 11.125rem;
+    line-height: 3.625rem;
+    padding: 3.75rem 6.25rem;
+    border-bottom: 1px solid #434343;
+    a {
+      height: 58px;
+      line-height: 58px;
+      color: #fff;
+      font-size: 1.75rem;
+      i {
+        font-size: 3.125rem;
+        vertical-align: middle;
+        margin-right: 0.625rem;
+        font-weight: 100;
+      }
+      span {
+        vertical-align: middle;
+        text-shadow: 0 0 1px #333;
+      }
+    }
+  }
+  .copyright {
+    height: 10.625rem;
+    padding-top: 2.5rem;
+    text-align: center;
+    color: #999;
+    font-size: 15px;
+    p {
+      line-height: 1;
+      margin-bottom: 1.25rem;
+    }
+    a {
+      color: #999;
+      line-height: 1;
+      padding: 0 0.625rem;
+      border-right: 1px solid #999;
+      &:last-child {
+        border-right: none;
+      }
+    }
+  }
+}
+@media (max-width: 991.98px) {
+  .app-footer .slogan {
+    height: 15rem;
+    padding: 0 3.75rem 1rem 3.75rem;
+    a {
+      font-size: 1.5rem;
+      margin-top: 1rem;
+      i {
+        font-size: 3rem;
+      }
+    }
+  }
 }
 </style>
