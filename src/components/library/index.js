6d7677c04a2de2b2c34ba1bc1e0ace4b0366ebb8
SheepNDW <hank3681@gmail.com>
2022-03-08 23:18:44 +0800

    改成自動化匯入元件

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,15 +1,22 @@
 // 插件 - Plugin
 // vue3.0插件的寫法要素: 匯出一個物件, 有install函式, 默認傳入app應用實例, app基礎之上擴展
 
-import GoPagination from './go-pagination.vue'
-import GoCarousel from './go-carousel.vue'
 import Message from './Message'
+
+// # 自動載入所有 library 下的元件
+// 使用 require 提供的函式 context 載入某一个目錄下的所有 .vue 後綴的檔案
+// context(目錄路徑, 是否加載子目錄, 載入檔案的正規表達式配對)
+const importFn = require.context('./', false, /\.vue$/)
 
 export default {
   install(app) {
-    // 註冊全域元件
-    app.component(GoPagination.name, GoPagination)
-    app.component(GoCarousel.name, GoCarousel)
+    // 根據 keys() 拿到所有匯入的檔案實現批量註冊
+    importFn.keys().forEach(path => {
+      // 匯入元件
+      const component = importFn(path).default
+      // 註冊為全域元件
+      app.component(component.name, component)
+    })
 
     // 定義原型函式
     app.config.globalProperties.$message = Message
