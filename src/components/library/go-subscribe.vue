6cb3e043c37a39b6a1ef224fcae2df2f65bcad88
SheepNDW <hank3681@gmail.com>
2022-03-09 14:23:35 +0800

    完成 about us 頁面展示

---
+++ src/components/library/go-subscribe.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="subscribe d-flex justify-content-center align-items-center">
+    <div class="container">
+      <h2 class="fs-5 text-center text-white mb-3">
+        {{
+          isSubscribe
+            ? '感謝您的訂閱! 這是您的折扣碼'
+            : '訂閱我們獲得超值折扣碼!'
+        }}
+      </h2>
+      <div class="w-50 mx-auto input-group" v-if="isSubscribe">
+        <input
+          type="text"
+          class="col-4 form-control text-primary"
+          readonly
+          value="keepgoforever"
+        />
+        <button class="col-2 btn btn-secondary" disabled type="button">
+          已訂閱
+        </button>
+      </div>
+      <form @submit="handlerSubscribe" class="w-50 mx-auto input-group" v-else>
+        <input
+          name="信箱"
+          type="email"
+          class="col-4 form-control"
+          placeholder="Email"
+        />
+        <button
+          class="col-2 btn btn-primary"
+          type="submit"
+          id="button-subscribe"
+        >
+          訂閱
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'GoSubscribe',
+  setup() {
+    const isSubscribe = ref(false)
+
+    const handlerSubscribe = () => {
+      isSubscribe.value = true
+    }
+
+    return { isSubscribe, handlerSubscribe }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.subscribe {
+  background: url('https://storage.googleapis.com/vue-course-api.appspot.com/sheep-api/1646805748268.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=TPShKCN0TKXnlcPEHjt%2BiWYkbv%2FmMGvVG4USzCe5rvWOpPnTTELS9l5kyLAtuVp1Z95J4Tk22wHc%2B9JP%2FfDPmIylfk%2FFB%2FJn6W05jj2Rr7HnnstR5T2VhIOZDsq%2BzHCsoM5n%2BaQundC5QlrLvIkqdZtHXOezTv6ZKu%2ByWLS8F2odEEZq0RfOat9KAzIVVIyVFNYlEgSgsdzZn4erYiDOOWrqlbU9H3UDZn64UqqHQdiLfJntt%2FyuSaCbpincmONuFshYvpJGiZJNt0xUKk%2BW0wKH2HvWVtNdtdz%2BT2PNd%2FbnnFbgrEkFiixPJ4EuLzufPB%2F6uP%2B7i6PNswCRn%2Fgp2g%3D%3D')
+    center center no-repeat;
+  height: 400px;
+  background-size: cover;
+  background-attachment: fixed;
+  @media (max-width: 767.98px) {
+    height: 250px;
+    background-attachment: unset;
+  }
+}
+</style>
--- src/views/front/about/Index.vue
+++ src/views/front/about/Index.vue
@@ -1,17 +1,151 @@
 <template>
   <div class="container">
+    <!-- banner -->
+    <div class="about-banner position-relative">
+      <h2
+        class="h2 bg-subTitle text-center text-light position-absolute px-4 py-3"
+      >
+        關於我們
+      </h2>
+    </div>
     <!-- 麵包屑 -->
     <GoBread>
       <GoBreadItem to="/">首頁</GoBreadItem>
       <GoBreadItem>關於我們</GoBreadItem>
     </GoBread>
-    <!-- banner -->
-    <!-- about us -->
   </div>
+  <!-- about us -->
+  <div class="bg-white">
+    <div class="about-us d-none d-sm-block container position-relative">
+      <div class="about-img">
+        <img
+          src="https://storage.googleapis.com/vue-course-api.appspot.com/sheep-api/1646557433177.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=k13iaKMI0ngy%2FR6Zxl2CW%2Fgk1loEdXKhHwhpoUU%2FZD4OnTA8Dxp9MrWyJOhJ4Dm7y5pLL97bFvKJWBtce0%2Bhwdo8LGNM1LYuccxg5bjmA%2FVjLBs3%2Be5P8FW%2Foc%2F2BvW0TggrYpqjr2IlIVnqX%2FAdDW9c8Te4QhY2vZQLmiDUx0z%2BIRGF0lUsCHGToTjclzRXYYR97qZmsHBPvzTVdQRb4rkTS7tx%2BnGMn3aOdEM6dZe797uW0LH42%2FZZUTOuvEsF17p5keHc%2Bg7EaAj7wCGeZP58tUYw8Qf6Du6wReOYfgA6JgrgVAD055ZhB83NMcfgJX95ZQrAdZangCH0Jq4ILw%3D%3D"
+          alt="about"
+          class="img-fiuld rounded"
+        />
+      </div>
+      <div class="about-content card">
+        <div class="card-body bg-gray-200 text-center">
+          <h5 class="card-title text-primary fw-bold">關於碁人館</h5>
+          <p class="card-text">我們是由一群圍棋愛好者所組成</p>
+          <p class="card-text">
+            在這裡的每一個人都熱愛著圍棋 追求棋藝的同時也很講究所使用的棋具
+          </p>
+          <p class="card-text">就讓我們為您提供最適合您的棋具吧!!</p>
+        </div>
+      </div>
+    </div>
+    <!-- 手機模式 -->
+    <div class="about-us d-sm-none row container mx-auto">
+      <div class="about-content-sm col-12 my-auto">
+        <img
+          src="https://storage.googleapis.com/vue-course-api.appspot.com/sheep-api/1646557433177.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=k13iaKMI0ngy%2FR6Zxl2CW%2Fgk1loEdXKhHwhpoUU%2FZD4OnTA8Dxp9MrWyJOhJ4Dm7y5pLL97bFvKJWBtce0%2Bhwdo8LGNM1LYuccxg5bjmA%2FVjLBs3%2Be5P8FW%2Foc%2F2BvW0TggrYpqjr2IlIVnqX%2FAdDW9c8Te4QhY2vZQLmiDUx0z%2BIRGF0lUsCHGToTjclzRXYYR97qZmsHBPvzTVdQRb4rkTS7tx%2BnGMn3aOdEM6dZe797uW0LH42%2FZZUTOuvEsF17p5keHc%2Bg7EaAj7wCGeZP58tUYw8Qf6Du6wReOYfgA6JgrgVAD055ZhB83NMcfgJX95ZQrAdZangCH0Jq4ILw%3D%3D"
+          class="card-img-top"
+          alt="about"
+        />
+        <div class="card-body bg-gray-200">
+          <h5 class="card-title text-primary fw-bold">關於碁人館</h5>
+          <p class="card-text">我們是由一群圍棋愛好者所組成</p>
+          <p class="card-text">
+            在這裡的每一個人都熱愛著圍棋 追求棋藝的同時也很講究所使用的棋具
+          </p>
+          <p class="card-text">就讓我們為您提供最適合您的棋具吧!!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- 訂閱 -->
+  <GoSubscribe />
 </template>
-
 <script>
 export default {
   name: 'About'
 }
 </script>
+
+<style lang="scss" scoped>
+.bg-subTitle {
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.about-banner {
+  background: url(https://storage.googleapis.com/vue-course-api.appspot.com/sheep-api/1646796873030.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=Ngy3Nihlqd4yd9GJ0ARt%2BfbB6p2GvhemWH%2Fd2MfKIKuB%2BzK7F1QFYhlOuSVi4UaM%2FiNnvdKfRn8c9JJkNpU0UV1GlWoCyyySh4ULysOHBW3Bd36LZekr6NoR4ODdPBpYVkACyiepWs5CK%2BAzJkCkyCejO1OIoXF9JFdSuqxJdcWw6IzhRzXxcQc2O4x4Lx2ybvTFejhyklgW%2FQrg8ftEWmqIKrOW0niysYqfgGcrLv2AUw1bZjy%2FDVutxYFCivpkUZ7nkSOWbwG0XOjWrK3Pcjbxr1wvMKUdNdWUub66jhsmrMElv8Qf4dDpajUa6s6SU7ZKdkK6DHUzSvjPC6o4Rg%3D%3D);
+
+  height: 400px;
+  position: relative;
+  background-size: cover;
+  background-position: center center;
+  opacity: 0.8;
+  border-radius: 1rem;
+  @media (max-width: 767.98px) {
+    height: 250px;
+  }
+}
+
+.about-us {
+  height: 600px;
+  @media (max-width: 767.98px) {
+    height: 550px;
+  }
+  @media (max-width: 575.98px) {
+    height: 500px;
+  }
+}
+.bg-gray-200 {
+  background: #e9ecef;
+}
+
+.about-img {
+  position: absolute;
+  top: 10%;
+  left: 10%;
+  z-index: 2;
+  img {
+    width: 50%;
+  }
+  @media (max-width: 1199.98px) {
+    top: 5%;
+    left: 5%;
+  }
+  @media (max-width: 991.98px) {
+    left: 0;
+  }
+  @media (max-width: 767.98px) {
+    img {
+      width: 100%;
+    }
+  }
+  @media (max-width: 575.98px) {
+    display: none;
+  }
+}
+
+.about-content {
+  position: absolute;
+  width: 50%;
+  top: 55%;
+  left: 50%;
+  @media (max-width: 1199.98px) {
+    top: 40%;
+    left: 45%;
+  }
+  @media (max-width: 991.98px) {
+    top: 35%;
+  }
+  @media (max-width: 767.98px) {
+    top: 60%;
+    left: 0;
+    width: 100%;
+  }
+  @media (max-width: 575.98px) {
+    display: none;
+  }
+}
+
+.about-content-sm {
+  height: 400px;
+}
+</style>
