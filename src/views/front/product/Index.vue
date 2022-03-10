6f2d9d8a2075d9c8aaf767038aa16fad21a8e61c
SheepNDW <hank3681@gmail.com>
2022-03-10 18:13:20 +0800

    完成產品列表頁面開發

--- src/views/front/product/Index.vue
+++ src/views/front/product/Index.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="banner">
+    <div class="container">
+      <div class="h-100 p-5">
+        <h2 class="text-center text-primary fw-bold my-5">線上商城</h2>
+      </div>
+    </div>
+  </div>
   <div class="container">
     <!-- 麵包屑 -->
     <GoBread>
@@ -8,6 +15,7 @@
     <!-- 產品列表 -->
     <ProductsList />
   </div>
+  <GoSubscribe />
 </template>
 
 <script>
@@ -17,3 +25,10 @@ export default {
   components: { ProductsList }
 }
 </script>
+
+<style lang="scss" scoped>
+.banner {
+  background: url(https://storage.googleapis.com/vue-course-api.appspot.com/sheep-api/1646906649618.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=CWlLx4ZRvvK%2BaQ3lkj74ddJ6n244DAp7h3UalpnmuJMwkEFDOcgnC0tUAcDWrDr1wuOghzELVHJFt5ARZiEbrk0PV6qDs2132NCd8gam1ShYiREEUNO1oWKM1B4gWbiVlDgrH67uhongS4aoHwKi%2BMhwzNgjVbZPyyli5sXfejvINdASvrlai2f%2F6A0ENQApZokKTMPz3kKucjXfr4WYFpGoI7ulsuCLAFn7b%2FwS2oEmMZ4eUFxEcpusJWHPNpEvqF7uzUMQ%2FjaVaLzbKNi8viDufCpukNY8iTt9XdukL6cBQQHlybOX%2BBKuO5XCwgNt4Xd1%2F%2Ft4VWHm%2Fh3N69JKLA%3D%3D)
+    center center / cover;
+}
+</style>
