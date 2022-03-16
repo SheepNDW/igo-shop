b722f996ce2209a9e10a10c8f4a8e531b73580c9
Sheep <hank3681@gmail.com>
2022-03-16 21:26:19 +0800

    update: 修改 upload 的判斷式讓它可以複用在別處

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -18,7 +18,7 @@ export const useUpload = () => {
 
   const upload = async () => {
     // 判斷是否有上傳圖片
-    if (!uploadInputRef.value.files[0]) return Message({ text: '請先上傳圖片' })
+    if (!file.value) return Message({ text: '請先上傳圖片' })
 
     const formData = new FormData()
     formData.append('file-to-upload', file.value)
